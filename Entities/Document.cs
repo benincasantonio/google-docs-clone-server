47741d8caa1f5b8de7407b5813d634fb73afd47c
Antonio Benincasa <benincasaantonio98@gmail.com>
2022-12-07 23:12:09 +0100

    feat: make content and title required

--- Entities/Document.cs
+++ Entities/Document.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json;
 
 namespace GoogleDocsCloneServer.Entities
@@ -6,7 +7,9 @@ namespace GoogleDocsCloneServer.Entities
     public class Document
     {
         public Guid Id { get; set; }
+        [Required]
         public string? Title { get; set; }
+        [Required]
         public JsonDocument? Content { get; set; }
     }
 }
---
+++ Migrations/20221207221136_Title_Content_Required.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoogleDocsCloneServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class TitleContentRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Documents",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<JsonDocument>(
+                name: "Content",
+                table: "Documents",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(JsonDocument),
+                oldType: "jsonb",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Documents",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<JsonDocument>(
+                name: "Content",
+                table: "Documents",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(JsonDocument),
+                oldType: "jsonb");
+        }
+    }
+}
